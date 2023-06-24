1d9f4fbc209f6bef46002b2c57ccd11f7ffa1f7d
Michelle-MingxuanWang <mingxuan.wang@outlook.com>
2023-06-23 23:07:56 -0700

    move files to folder

---
+++ backend/src/main/java/dev/michellemw/movies/MovieService.java
@@ -0,0 +1,24 @@
+package dev.michellemw.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor // constructor for final arguments
+public class MovieService {
+    private final MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
---
+++ backend/src/main/java/dev/michellemw/movies/ReviewController.java
@@ -0,0 +1,23 @@
+package dev.michellemw.movies;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+    private final ReviewService service;
+
+    @PostMapping()
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+
+        return new ResponseEntity<Review>(service.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.OK);
+    }
+}
